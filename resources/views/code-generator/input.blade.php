d1f009ced943aae38499b6b2d188e6ca55f73796
thipaporn <35359046+thipaporn@users.noreply.github.com>
2020-10-20 21:28:29 +0700

    edit bar

--- resources/views/code-generator/input.blade.php
+++ resources/views/code-generator/input.blade.php
@@ -98,14 +98,4 @@
       window.location.assign("{{URL::to('codeGeneratorOutput/?bill=')}}" + billNum);
     }
   </script>
-  <!-- endinject -->
-  <!-- Plugin js for this page -->
-  <!-- End plugin js for this page -->
-  <!-- inject:js -->
-  <script src="assets/js/off-canvas.js"></script>
-  <script src="assets/js/hoverable-collapse.js"></script>
-  <script src="assets/js/misc.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page -->
-  <!-- End custom js for this page -->
   @endsection
