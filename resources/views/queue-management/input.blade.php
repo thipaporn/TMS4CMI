449ef652b2e20990ceaa1ae3015afc3d2379c864
wavewcy <witchayawave@gmail.com>
2020-10-21 14:28:28 +0700

    save

--- resources/views/queue-management/input.blade.php
+++ resources/views/queue-management/input.blade.php
@@ -71,7 +71,6 @@
               <div class="col-md-12"><br /></div>
             </div>
           </form>
-
           <div class="row">
             <div class="col-md-4"></div>
             <div class="col-md-4">
