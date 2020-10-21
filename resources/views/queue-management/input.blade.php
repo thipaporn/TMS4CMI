b4185ef89af28f99ca4f367a74975ced531575ec
wavewcy <witchayawave@gmail.com>
2020-10-21 10:49:53 +0700

    edit css input queue

--- resources/views/queue-management/input.blade.php
+++ resources/views/queue-management/input.blade.php
@@ -17,6 +17,7 @@
                       <div class="form-group">
                         <label for="type">ประเภทของรถขนส่ง</label>
                         <select class="form-control" id="type">
+                          <option selected value="0">เลือก</option>
                           <option value="รถกระบะ">รถกระบะ</option>
                           <option value="รถบรรทุก">รถบรรทุก</option>
                         </select>
@@ -24,6 +25,7 @@
                       <div class="form-group">
                         <label for="location">เส้นทางในการขนส่ง</label>
                         <select class="form-control" id="location">
+                          <option selected value="0">เลือก</option>
                           <option value="เชียงใหม่">เชียงใหม่</option>
                           <option value="เชียงดาว-ฝาง">เชียงดาว-ฝาง</option>
                           <option value="ลำปาง">ลำปาง</option>
@@ -60,17 +62,18 @@
                         </div>  
  
                       <div class="row">
+                        <div class="col-md-12"><br /></div>
                         <div class="col-md-3"></div>
                         <div class="col-md-6">
-                          <button type="submit" style="width:50%;font-size:20px;" class="btn btn-gradient-primary mr-2">เพิ่มคิวรถ</button>
+                          <button type="submit" style="width:100%;font-size:20px;" class="btn btn-gradient-primary mr-2">เพิ่มคิวรถ</button>
                         </div>
                         <div class="col-md-3"></div>
                         <div class="col-md-12"><br /></div>
                         <div class="col-md-3"></div>
-                        <div class="col-md-12">
-                          <button style="width:50%;font-size:20px;" class="btn btn-light">ยกเลิก</button>
+                        <div class="col-md-6">
+                          <button style="width:100%;font-size:20px;" class="btn btn-light" onClick="javascript:history.go(-1)">ยกเลิก</button>
                         </div>
-                          <div class="col-md-3"></div>
+                        <div class="col-md-3"></div>
                       </div>
  
                     </form>
@@ -87,25 +90,25 @@
 $(document).ready(function(){
         var i=1;
         $('.btn_add').click(function(){
-                i++;
-    $('.dynamic_field').append('<div class="row"><div class="col-md-5"><div class="form-group"><input type="text" name="name[]" class="form-control" id="name" ></div></div><div class="col-md-5"><div class="form-group"><input type="text" name="number[]" class="form-control" id="number" ></div></div><div class="col-md-2"><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">ลบ</button></div></div>');
-        });
-       
+          i++;
+        $('.dynamic_field').append('<div class="row"><div class="col-md-5"><div class="form-group"><input type="text" name="name[]" class="form-control" id="name" ></div></div><div class="col-md-5"><div class="form-group"><input type="text" name="number[]" class="form-control" id="number" ></div></div><div class="col-md-2"><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">ลบ</button></div></div>');
+            });
+            
         $('.dynamic_field').on('click', '.btn_remove', function(){
-    $(this).closest('div.row').remove();
+          $(this).closest('div.row').remove();
         });
        
         $('#submit').click(function(){         
-                $.ajax({
-                        url:"name.php",
-                        method:"POST",
-                        data:$('#add_bill').serialize(),
-                        success:function(data)
-                        {
-                                alert(data);
-                                $('#add_bill')[0].reset();
-                        }
-                });
+          $.ajax({
+            url:"name.php",
+            method:"POST",
+            data:$('#add_bill').serialize(),
+            success:function(data)
+            {
+              alert(data);
+              $('#add_bill')[0].reset();
+            }
+          });
         });
        
 });
