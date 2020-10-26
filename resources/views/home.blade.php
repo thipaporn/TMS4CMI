2d6a922c53b0a3e1c66c4edf0bee1aa0fbf8ed89
wavewcy <witchayawave@gmail.com>
2020-10-26 13:48:26 +0700

    search

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
     <h3>รายการทั้งหมด</h3>
   </div>
   <div class="col-lg-6 template-demo">
-    <form action="queueManagement" method="get">
+    <form action="home" method="get">
       <div class="form-group row">
         <div class="col-lg-8">
           <input type="text" name="search" class="form-control" placeholder="ค้นหาข้อมูล">
@@ -26,6 +26,17 @@
       <div class="card-body">
         <table class="table table-striped">
           <thead>
+            @if(isset($_GET['search']))
+            <?php
+            $search = strtolower($_GET['search']);
+            $row = 0;
+            ?>
+            @foreach($orders as $index => $order)
+            @if(str_contains(strtolower($order->name), $search) || str_contains(strtolower($order->type), $search)|| str_contains(strtolower($order->carNumber), $search)|| str_contains(strtolower($order->dest), $search) || str_contains(strtolower($order->trackNumber), $search) || str_contains(strtolower($order->startDate), $search)|| str_contains(strtolower($order->status), $search))
+            <?php $row++; ?>
+            @endif
+            @endforeach
+            @if ($row != 0)
             <tr>
               <th>ลำดับ</th>
               <th>ชื่อพนักงานขับรถ</th>
@@ -36,9 +47,29 @@
               <th>วันที่จัดส่ง</th>
               <th>สถานะการขนส่ง</th>
             </tr>
+            @endif  
+            @else
+            <tr>
+              <th>ลำดับ</th>
+              <th>ชื่อพนักงานขับรถ</th>
+              <th>ประเภทรถ</th>
+              <th>หมายเลขรถ</th>
+              <th>สถานที่</th>
+              <th>เลขที่บิล</th>
+              <th>วันที่จัดส่ง</th>
+              <th>สถานะการขนส่ง</th>
+            </tr>
+            @endif
           </thead>
           <tbody>
+          @if (isset($_GET['search']))
+          <?php
+          $search = strtolower($_GET['search']);
+          $row = 0;
+          ?>
           @foreach($orders as $index => $order)
+          @if(str_contains(strtolower($order->name), $search) || str_contains(strtolower($order->type), $search)|| str_contains(strtolower($order->carNumber), $search)|| str_contains(strtolower($order->dest), $search) || str_contains(strtolower($order->trackNumber), $search) || str_contains(strtolower($order->startDate), $search)|| str_contains(strtolower($order->status), $search))
+          <?php $row++; ?>
             <tr>
               <td class="py-1">{{$index + 1}}</td>
               <td>{{$order->name}}</td>
@@ -49,7 +80,27 @@
               <td>{{$order->startDate}}</td>
               <td>{{$order->status}}</td>
             </tr>
+          @endif
           @endforeach
+          @if ($row = 0)
+          <tr>
+            <td class="py-1">Not Found</td>
+          </tr>
+          @endif
+          @else
+          @foreach($orders as $index => $order)
+          <tr>
+            <td class="py-1">{{$index + 1}}</td>
+            <td>{{$order->name}}</td>
+            <td>{{$order->type}}</td>
+            <td>{{$order->carNumber}}</td>
+            <td>{{$order->dest}}</td>
+            <td>{{$order->trackNumber}}</td>
+            <td>{{$order->startDate}}</td>
+            <td>{{$order->status}}</td>
+          </tr>
+          @endforeach
+          @endif
           </tbody>
         </table>
       </div>
--- resources/views/queue-management/output.blade.php
+++ resources/views/queue-management/output.blade.php
@@ -9,7 +9,7 @@
     <h3>จัดคิวรถขนส่ง</h3>
   </div>
   <div class="col-lg-6 template-demo">
-    <form action="queueManagement" method="post">
+    <form action="queueManagement" method="get">
       <div class="form-group row">
         <div class="col-lg-8">
           <input type="text" name="search" class="form-control" placeholder="ค้นหาข้อมูล">
@@ -20,10 +20,11 @@
       </div>
     </form>
   </div>
-  <?php
-    //echo $pre;
-  ?>
-  <form action="addQueue" method="post">
+</div>
+<?php
+  //echo $pre;
+?>
+<form action="addQueue" method="post">
     @csrf
   <div class="col-lg-12 grid-margin stretch-card">
     <div class="card">
@@ -37,7 +38,7 @@
               <th>หมายเลขรถ</th>
               <th>สถานที่</th>
               <th>เลขที่บิล</th>
-            </tr>
+            </tr>            
           </thead>
           <tbody>
             <?php
@@ -53,45 +54,216 @@
                 }
                 //print_r($driverDist);
             ?>
-            @foreach($pre as $row)
+            
+            @if (isset($_GET['search']))
+              <?php
+              $search = strtolower($_GET['search']);
+              $r= 0;
+              ?>
+              @foreach($pre as $row)
                 <?php
-                    $driverCan = array();
-                    foreach ($driver as $d) {
-                        $driverCan[$d->id] = true;
-                        if($row->type == $d->type){
-                            foreach ($schedule as $s) {
-                                if($d->id == $s->id &&  $row->startDate == $s->startDate){
-                                    $driverCan[$d->id] = false;
-                                }
-                            }
-                        }else{
-                            $driverCan[$d->id] = false;
-                        }
-                    }
+                  $driverCan = array();
+                  foreach ($driver as $d) {
+                      $driverCan[$d->id] = true;
+                      if($row->type == $d->type){
+                          foreach ($schedule as $s) {
+                              if($d->id == $s->id &&  $row->startDate == $s->startDate){
+                                  $driverCan[$d->id] = false;
+                              }
+                          }
+                      }else{
+                          $driverCan[$d->id] = false;
+                      }
+                  }
 
-                    //id driver can order
-                    $driverId = array();
-                    foreach ($driver as $d) {
-                        if($driverCan[$d->id]){
-                            array_push($driverId,$d->id);
-                        }
-                    }
-                    // echo " have ";
-                    // print_r($driverId);
+                  //id driver can order
+                  $driverId = array();
+                  foreach ($driver as $d) {
+                      if($driverCan[$d->id]){
+                          array_push($driverId,$d->id);
+                      }
+                  }
+                  // echo " have ";
+                  // print_r($driverId);
 
-                    if(count(array_filter($driverCan)) == 1){
-                        foreach ($driver as $d) {
-                            if($driverCan[$d->id]){
-                                $id = $d->id;
-                                $driverName = $d->name;
-                                $driverNumber = $d->number;
-                                foreach ($dist as $distValue) {
-                                    if($distValue->prov == $row->dest){
-                                        $driverDist[$d->id] += $distValue->dist;
-                                    }
-                                }
-                            }
-                        }
+                  if(count(array_filter($driverCan)) == 1){
+                      foreach ($driver as $d) {
+                          if($driverCan[$d->id]){
+                              $id = $d->id;
+                              $driverName = $d->name;
+                              $driverNumber = $d->number;
+                              foreach ($dist as $distValue) {
+                                  if($distValue->prov == $row->dest){
+                                      $driverDist[$d->id] += $distValue->dist;
+                                  }
+                              }
+                          }
+                      }
+                  }elseif(count(array_filter($driverCan)) == 2){
+                      $cal = array();
+                      foreach ($dist as $distValue) {
+                          if($distValue->prov == $row->dest){
+                              $a = $driverDist[$driverId[0]];
+                              $b = $driverDist[$driverId[1]];
+                              $x = ($a + $b + $distValue->dist)/2;
+
+                              $cal[$driverId[0]] = pow(($a + $distValue->dist - $x),2) + pow(($b - $x),2);
+                              $cal[$driverId[1]] = pow(($a - $x),2) + pow(($b + $distValue->dist - $x),2);
+
+                              $cal[$driverId[0]] = sqrt($cal[$driverId[0]]);
+                              $cal[$driverId[1]] = sqrt($cal[$driverId[1]]);
+
+                              foreach ($driver as $d) {
+                                  if( $cal[$driverId[0]] <= $cal[$driverId[1]] ){
+                                      if($d->id == $driverId[0]){
+                                          $id = $d->id;
+                                          $driverName = $d->name;
+                                          $driverNumber = $d->number;
+                                          $driverDist[$driverId[0]] += $distValue->dist;
+                                      }
+                                  }elseif( $cal[$driverId[0]] > $cal[$driverId[1]] ){
+                                      if($d->id == $driverId[1]){
+                                          $id = $d->id;
+                                          $driverName = $d->name;
+                                          $driverNumber = $d->number;
+                                          $driverDist[$driverId[1]] += $distValue->dist;
+                                      }
+                                  }
+                              }
+                          }
+                      }
+                      // echo "cal ----> ";
+                      // print_r($cal);
+                  }elseif(count(array_filter($driverCan)) == 3){
+                      $cal = array();
+                      foreach ($dist as $distValue) {
+                          if($distValue->prov == $row->dest){
+                              $a = $driverDist[$driverId[0]];
+                              $b = $driverDist[$driverId[1]];
+                              $c = $driverDist[$driverId[2]];
+                              $x = ($a + $b + $c +$distValue->dist)/3;
+
+                              $cal[$driverId[0]] = pow(($a + $distValue->dist - $x),2)  + pow(($b - $x),2) + pow(($c - $x),2);
+                              $cal[$driverId[1]] = pow(($a - $x),2)  + pow(($b + $distValue->dist - $x),2) + pow(($c - $x),2);
+                              $cal[$driverId[2]] = pow(($a - $x),2)  + pow(($b - $x),2) + pow(($c + $distValue->dist - $x),2);
+
+                              $cal[$driverId[0]] = sqrt($cal[$driverId[0]]/2 );
+                              $cal[$driverId[1]] = sqrt($cal[$driverId[1]]/2 );
+                              $cal[$driverId[2]] = sqrt($cal[$driverId[2]]/2 );
+
+                              foreach ($driver as $d) {
+                                  if( $cal[$driverId[0]] <= $cal[$driverId[1]] && $cal[$driverId[0]] <= $cal[$driverId[2]] ){
+                                      if($d->id == $driverId[0]){
+                                          $id = $d->id;
+                                          $driverName = $d->name;
+                                          $driverNumber = $d->number;
+                                          $driverDist[$driverId[0]] += $distValue->dist;
+                                      }
+                                  }elseif( $cal[$driverId[1]] <= $cal[$driverId[0]] && $cal[$driverId[1]] <= $cal[$driverId[2]]){
+                                      if($d->id == $driverId[1]){
+                                          $id = $d->id;
+                                          $driverName = $d->name;
+                                          $driverNumber = $d->number;
+                                          $driverDist[$driverId[1]] += $distValue->dist;
+                                      }
+                                  }elseif( $cal[$driverId[2]] <= $cal[$driverId[0]] && $cal[$driverId[2]] <= $cal[$driverId[2]]){
+                                      if($d->id == $driverId[2]){
+                                          $id = $d->id;
+                                          $driverName = $d->name;
+                                          $driverNumber = $d->number;
+                                          $driverDist[$driverId[2]] += $distValue->dist;
+                                      }
+                                  }
+                              }
+                          }
+                      }
+                      // echo "cal ----> ";
+                      // print_r($cal);
+                  }else{
+                      foreach ($driver as $d) {
+                          if($d->id == "001"){
+                              $driverId = $d->id;
+                              $driverName = $d->name;
+                              $driverNumber = $d->number;
+                              foreach ($dist as $distValue) {
+                                  if($distValue->prov == $row->dest){
+                                      $driverDist[$d->id] += $distValue->dist;
+                                  }
+                              }
+                          }
+                      }
+                  }
+                  //print_r($driverDist);
+                ?>
+                @if(str_contains(strtolower($driverName), $search) || str_contains(strtolower($row->type), $search) || str_contains(strtolower($driverNumber), $search) || str_contains(strtolower($row->dest), $search) || str_contains(strtolower($row->trackNumber), $search))
+                  <?php $r++; ?>
+                  <tr>
+                      <td>{{$count}}</td>
+                      <td>{{$driverName}}</td>
+                      <td>{{$row->type}}</td>
+                      <td>{{$driverNumber}}</td>
+                      <td>{{$row->dest}}</td>
+                      <td>{{$row->trackNumber}}</td>
+                  </tr>
+                  <input type="hidden" name="id[]" value="{{$id}}">
+                  <input type="hidden" name="name[]" value="{{$driverName}}">
+                  <input type="hidden" name="type[]" value="{{$row->type}}">
+                  <input type="hidden" name="carNumber[]" value="{{$driverNumber}}">
+                  <input type="hidden" name="dest[]" value="{{$row->dest}}">
+                  <input type="hidden" name="trackNumber[]" value="{{$row->trackNumber}}">
+                  <input type="hidden" name="startDate[]" value="{{$row->startDate}}">
+                  @foreach ($dist as $d)
+                    @if ($d->prov == $row->dest)
+                      <input type="hidden" name="dist[]" value="{{$d->dist}}">
+                    @endif
+                  @endforeach
+                  <?php $count++ ?>
+                @endif              
+              @endforeach
+            
+            @if ($r = 0)
+              <h3>Not Found {{$_GET['search']}}</h3>
+            @endif
+            @else
+              @foreach($pre as $row)
+                <?php
+                  $driverCan = array();
+                  foreach ($driver as $d) {
+                      $driverCan[$d->id] = true;
+                      if($row->type == $d->type){
+                          foreach ($schedule as $s) {
+                              if($d->id == $s->id &&  $row->startDate == $s->startDate){
+                                  $driverCan[$d->id] = false;
+                              }
+                          }
+                      }else{
+                          $driverCan[$d->id] = false;
+                      }
+                  }
+
+                  //id driver can order
+                  $driverId = array();
+                  foreach ($driver as $d) {
+                      if($driverCan[$d->id]){
+                          array_push($driverId,$d->id);
+                      }
+                  }
+                  // echo " have ";
+                  // print_r($driverId);
+
+                  if(count(array_filter($driverCan)) == 1){
+                      foreach ($driver as $d) {
+                          if($driverCan[$d->id]){
+                              $id = $d->id;
+                              $driverName = $d->name;
+                              $driverNumber = $d->number;
+                              foreach ($dist as $distValue) {
+                                  if($distValue->prov == $row->dest){
+                                      $driverDist[$d->id] += $distValue->dist;
+                                  }
+                              }
+                          }
+                      }
                     }elseif(count(array_filter($driverCan)) == 2){
                         $cal = array();
                         foreach ($dist as $distValue) {
@@ -187,14 +359,14 @@
                         }
                     }
                     //print_r($driverDist);
-                ?>
+                ?>              
                 <tr>
-                    <td>{{$count}}</td>
-                    <td>{{$driverName}}</td>
-                    <td>{{$row->type}}</td>
-                    <td>{{$driverNumber}}</td>
-                    <td>{{$row->dest}}</td>
-                    <td>{{$row->trackNumber}}</td>
+                  <td>{{$count}}</td>
+                  <td>{{$driverName}}</td>
+                  <td>{{$row->type}}</td>
+                  <td>{{$driverNumber}}</td>
+                  <td>{{$row->dest}}</td>
+                  <td>{{$row->trackNumber}}</td>
                 </tr>
                 <input type="hidden" name="id[]" value="{{$id}}">
                 <input type="hidden" name="name[]" value="{{$driverName}}">
@@ -209,7 +381,9 @@
                   @endif
                 @endforeach
                 <?php $count++ ?>
-            @endforeach
+              @endforeach
+            @endif
+
           </tbody>
         </table>
       </div>
@@ -232,8 +406,7 @@
     </div>
     <div class="col-lg-4"></div>
   </div>
-  </form>
-</div>
+</form>
 
 @endsection
 @section('script')
